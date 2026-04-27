c7a555ba4e3b118f38fa36613912a6b77fb547f3
vanecarol <atravesdelawebsite@gmail.com>
2026-04-27 23:11:57 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MaternidadApp from './maternidad-abundancia.jsx' 
-import './index.css' // Asegúrate de que el archivo index.css esté en la misma carpeta
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
