8300fbcd3745cc6844a51728fc44d5eb68ad23ee
vanecarol <atravesdelawebsite@gmail.com>
2026-04-27 23:17:04 +0200

    Update and rename main.jsx to src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MaternidadApp from './maternidad-abundancia.jsx' 
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MaternidadApp />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
